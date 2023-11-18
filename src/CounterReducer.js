aa209ae4bad902f0315e2d9fdaf6bf9eca2775f5
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-11-18 15:29:48 +0500

    add defult in cr

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -2,7 +2,9 @@ const counterReducer = (state, action) => {
     switch (action.type) {
       case 'INCREMENT':
         return state + 1
+        default:
+          return state;
     }
-  }
+}
 
-  export default counterReducer;
+export default counterReducer;
